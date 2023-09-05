394a8fad961cd791427acf4294422e2e5bb566d1
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-09-05 20:29:46 +0300

    Identy işlemleri

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,8 +1,14 @@
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();// controller de "IHttpClientFactory" constroctor'ý kullandýðýmýz için bunu burada tanýmlarýz
+
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
