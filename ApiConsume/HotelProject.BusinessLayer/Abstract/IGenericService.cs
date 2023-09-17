d1e227a5b01864fdb6268d75cd2413f12ae47a7c
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-09-17 17:25:03 +0300

    Düzenlemeler Yapıldı

--- ApiConsume/HotelProject.BusinessLayer/Abstract/IGenericService.cs
+++ ApiConsume/HotelProject.BusinessLayer/Abstract/IGenericService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelProject.BusinessLayer.Abstract
+﻿namespace HotelProject.BusinessLayer.Abstract
 {
     public interface IGenericService<T> where T : class
     {
