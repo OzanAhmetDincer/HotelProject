7d5adfbf6930d1d553e0dcd10c53788c9aca7c97
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-10-15 16:56:23 +0300

    İletişim Paneli Bitti

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,15 +1,17 @@
 ﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ContactDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
-    public class BookingController : Controller
+    public class ContactController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public BookingController(IHttpClientFactory httpClientFactory)
+        public ContactController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -17,24 +19,23 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
-
         [HttpGet]
-        public PartialViewResult AddBooking()
+        public PartialViewResult SendMessage()
         {
             return PartialView();
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBooking(CreateBookingDto createBookingDto)
+        public async Task<IActionResult> SendMessage(CreateContactDto createContactDto)
         {
-            createBookingDto.Status = "Onay Bekliyor";// Her yapılan rezervasyon sonrası Statuz özelliği olarak bu gelecek ki onaylanması yapılsın. Direkt her  rezervasyon onaylanmasın"
+            createContactDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
             var client = _httpClientFactory.CreateClient();// Bir tane istemci oluşturduk
-            var jsonData = JsonConvert.SerializeObject(createBookingDto);// Burada ise yeni eklenecek olan bir veriyi json formatına dönüştürmemiz lazım. "SerializeObject" ile bu işlemi yaparız. oluşturduğumuz nesneyi bizim için Json string’e çevirdi.
+            var jsonData = JsonConvert.SerializeObject(createContactDto);// Burada ise yeni eklenecek olan bir veriyi json formatına dönüştürmemiz lazım. "SerializeObject" ile bu işlemi yaparız. oluşturduğumuz nesneyi bizim için Json string’e çevirdi.
 
             // "StringContent" içeriğin dönüşümü için kullanacağımız bir sınıf. Web api sunucusuna bir nesne göndermek istediğimde, HTTP içeriğine format eklemek için StringContent'i kullanırım, örneğin Müşteri nesnesini sunucuya json olarak eklemek için. StringContent sınıfı, http sunucu/istemci iletişimine uygun biçimlendirilmiş bir metin oluşturur. Bir istemci isteğinden sonra sunucu HttpResponseMessageve ile yanıt verecektir  bu yanıtın bir içeriğe ihtiyacı olacaktır bunu da StringContent sınıfla oluşturulabiliriz  
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");// jsonData ile içeriğimizi veririz, Encoding.UTF8 ile de Türkçe karakter destekleyecek şekilde getirdik mediaType ise application/json olacak. Veri, verinin kodlanmış hali ve türü
-            await client.PostAsync("http://localhost:12091/api/Booking", stringContent);// Ekleme yapabilmek için "PostAsync" metodunu kullanırız
-            return RedirectToAction("Index", "Default");
+            await client.PostAsync("http://localhost:12091/api/Contact", stringContent);// Ekleme yapabilmek için "PostAsync" metodunu kullanırız
+            return RedirectToAction("Index" , "Default");
         }
     }
 }
